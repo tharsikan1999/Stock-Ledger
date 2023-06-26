bb210551a558f1830342202337c7bbdadb30943b
Sathasivam Tharsikan <110738940+tharsikan1999@users.noreply.github.com>
2023-06-26 18:02:12 +0530

    Add files via upload

---
+++ Project/App.js
@@ -0,0 +1,12 @@
+const hangBurgar = document.querySelector(".hanburgar");
+
+const slideBar = document.querySelector(".slide-bar");
+
+
+
+hangBurgar.addEventListener("click",function(){
+    slideBar.classList.toggle("Display");
+    hangBurgar.children[0].classList.toggle("Display1");
+    hangBurgar.children[1].classList.toggle("Display2");
+    hangBurgar.children[2].classList.toggle("Display3");
+})
