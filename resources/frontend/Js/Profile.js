744e90804065dbc2f317bc3f71f4c7accc18e7cd
Dell <jithmarajapaksha00@gmail.com>
2023-07-05 19:39:01 +0530

    Profile added

---
+++ resources/frontend/Js/Profile.js
@@ -0,0 +1,26 @@
+
+<html>
+<head>
+  <title>User Profile</title>
+  <link rel="stylesheet" type="text/css" href="E:\apach frienda\htdocs\Stock-Ledger\resources\frontend\profile.css">
+</head>
+<body>
+  <div class="profile-container">
+    <img src="OIP.jpg" >
+    <div class="profile-info">
+      <label for="name"><p style="font-size:24px;">Name:</p></label>
+      <span id="name">John Doe</span>
+    </div>
+    <div class="profile-info">
+      <label for="role"><p style="font-size:24px;">Role:</p></label>
+      <span id="role">Administrator</span>
+    </div>
+    <div class="profile-info">
+      <label for="password"><p style="font-size:24px;"> Password:</p></label>
+      <span id="password">******</span><br> <br>
+      <input type="submit"  class="button" value="Edit" name="Edit">
+    </div>
+  </div>
+</body>
+</html>
+
